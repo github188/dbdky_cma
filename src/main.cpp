90e6707fb1b8a8f244c0291c2b4e0b118ddfbad7
kevin_neu <kevin_neu@163.com>
2013-11-05 14:16:39 +0800

    add startup shell

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <utils/Logging.h>
 #include <port/EventLoop.h>
 
+#include "ConfigUtil.h"
+
 using namespace dbdky;
 using namespace dbdky::port;
 using namespace dbdky::cma_server;
@@ -11,7 +13,10 @@ int main(int argc, char** argv)
 {
     LOG_INFO << "pid = " << getpid();
     EventLoop loop;
-    InetAddress listenAddr("127.0.0.1", 6001);
+//    InetAddress listenAddr("127.0.0.1", 6001);
+    InetAddress listenAddr(ConfUtil::getInstance()->getProxyIP(),
+			ConfUtil::getInstance()->getProxyPort());
+
     dbdky::cma_server::cma_server server(&loop, listenAddr, "cma_server");
     server.start();
     loop.loop();
